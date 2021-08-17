a96e0651e01934b1dda14931dd4de6cef8f20f9c
Prince Darko <padarko99@gamil.com>
2021-08-17 16:43:08 -0700

    done

--- index.js
+++ index.js
@@ -69,3 +69,20 @@ app.post('/send',urlencodedParser, (req, res)=>{
 app.listen(PORT, ()=> {
     console.log('we working!!!')
 })
+
+// let transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 465,
+//   secure: true,
+//   service: 'gmail',
+//   auth: {
+//         type: "OAUTH2",
+//         user: process.env.USER,  //set these in your .env file
+//         pass: process.env.PASS,
+//         clientId: process.env.OAUTH_CLIENT_ID,
+//         clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+//         accessToken: process.env.OAUTH_ACCESS_TOKEN,
+//         expires: 3599
+//   }
+// });
