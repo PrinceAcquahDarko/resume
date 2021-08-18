90848b7ef031ddbb607b6da64f6869a443bc15e5
Prince Darko <padarko99@gamil.com>
2021-08-17 23:54:15 -0700

    we push'

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express();
 const path = require('path');
 let bodyParser = require('body-parser')
 const User = require('./model/model')
-let cors = require('cors')
 let mongoose = require('mongoose');
 
 
 require('dotenv').config()
 
-url =  "mongodb+srv://Darko:gospel333@cluster0.xbklg.mongodb.net/zuri?retryWrites=true&w=majority"
+const url = process.env.URL
 
 mongoose.connect(url,  { useUnifiedTopology: true,  useNewUrlParser: true })
     .then((data) => console.log('connected to database successfully'))
@@ -21,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false})
 
-app.use(cors())
+
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -31,8 +30,8 @@ app.post('/send',urlencodedParser, (req, res)=>{
   const user = new User(req.body)
   user.save((err, user) => {
       if(err){
-         console.log(err)
-         return res.send({message: err})
+         // console.log(err)
+          return res.send({message: 'an error occured'})
       }
          
       return res.send({message: 'successful'})
@@ -44,9 +43,9 @@ app.post('/send',urlencodedParser, (req, res)=>{
 
 
 app.get('/results', (req, res)=>{
-    User.find((err, user) => {
+    User.find((err , user) => {
       if(err)
-          return res.send({err})
+          return res.send({message: 'an error occured'})
       return res.send({message: 'successful', user})
   })
 })
